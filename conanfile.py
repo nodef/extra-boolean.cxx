47fc96b2d5b425ed3c51310dffeb1050632d456d
Subhajit Sahu <wolfram77@gmail.com>
2021-04-10 18:22:44 +0530

    :microbe: use include dir

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile
 class ExtraBoolean(ConanFile):
   name = "extra-boolean"
   version = "0.0"
-  exports_sources = "src/*"
+  exports_sources = "include/*"
   no_copy_source = True
 
   def package(self):
